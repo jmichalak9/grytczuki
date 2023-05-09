68d6b5276c8c56e4e2e5ab69f146759fae499215
Piotr Kryczka <72410501+piotrek999@users.noreply.github.com>
2023-05-09 23:17:01 +0200

    Add IsFirstPlayerMove to IGame

--- AbelThueOnline/MCTS/IGame.cs
+++ AbelThueOnline/MCTS/IGame.cs
@@ -2,6 +2,16 @@
 
 public interface IGame
 {
+    /// <remarks>
+    /// If empty list then player lost.
+    /// </remarks>
     List<int> GetAvailableActions();
+    /// <remarks>
+    /// Returns new instance.
+    /// </remarks>
     IGame MakeMove(int action);
+    /// <remarks>
+    /// Needed to check who lost.
+    /// </remarks>
+    bool IsFirstPlayerMove { get; set; }
 }
