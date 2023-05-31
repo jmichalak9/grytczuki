1529fc17896e8324212f026696d9048abdf9cf32
adassimo25 <adasimbad99@gmail.com>
2023-05-31 03:17:19 +0200

    Add EXE directory

--- make-exe.bat
+++ make-exe.bat
@@ -1,2 +1,14 @@
+@echo off
+set folder=EXE
+if not exist "%folder%" (
+    mkdir "%folder%"
+    echo EXE directory created.
+) else (
+    echo EXE directory exists.
+)
+
 dotnet publish .\AbelThueOnline\AbelThueOnline\AbelThueOnline.csproj -r win-x64 -c Release -o . -p:DebugType=None -p:DebugSymbols=false -p:PublishReadyToRunShowWarnings=true -p:PublishReadyToRun=true -p:PublishSingleFile=true -p:PublishTrimmed=true --self-contained true -p:IncludeNativeLibrariesForSelfExtract=true
+
+echo AbelThueOnline.exe created in EXE directory.
+
 pause
