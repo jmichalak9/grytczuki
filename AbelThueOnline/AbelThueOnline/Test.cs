ea02a42bb476518ef4e2300ae2e1968d54c61c9c
Jakub Michalak <michalakjakub1999@gmail.com>
2023-06-06 11:08:17 +0200

    Use both sides in tests

--- AbelThueOnline/AbelThueOnline/Test.cs
+++ AbelThueOnline/AbelThueOnline/Test.cs
@@ -4,7 +4,7 @@ namespace AbelThueOnline;
 
 public class Test
 {
-    private int charsCountMin = 4;
+    private int charsCountMin = 3;
     private int charsCountMax = 15;
     private int wordLenMin = 3;
     private int wordLenMax = 15;
@@ -14,7 +14,7 @@ public class Test
     {
         for (int i = charsCountMin; i < charsCountMax; i++)
         {
-            for (int j = wordLenMin; j < wordLenMax; j++)
+             for (int j = wordLenMin; j < wordLenMax; j++)
             {
                 var score = RunSingleSetup(i, j, games);
                 Console.WriteLine($"n={i}, m={j}: {score}:{games-score}");
@@ -27,13 +27,17 @@ public class Test
         var p1win = 0;
         for (int i = 0; i < games; i++)
         {
-            Console.WriteLine($"playing game {i}/{games} for {charsCount}, {wordLen}");
             var game = new Game(charsCount, wordLen);
-            var player1 = new RandomAlgorithm();
-            var player2 = new MCTS(
+            IAlgorithm player1 = new RandomAlgorithm();
+            IAlgorithm player2 = new MCTS(
                 MCTS.Strategy.UCBTuned,
-                2000,
+                1000,
                 TimeSpan.FromSeconds(5));
+            var swap = i % 2 == 0;
+            if (swap)
+            {
+                (player1, player2) = (player2, player1);
+            }
             var gameInfo = new Program.GameInfo(game, player1, player2);
             game = gameInfo.Game;
             while (true)
@@ -44,7 +48,8 @@ public class Test
                 var availableActions = game.GetAvailableActions();
                 if (!availableActions.Any())
                 {
-                    if (game.GameState == GameState.Player2Move)
+                    if ((game.GameState == GameState.Player2Move && !swap) ||
+                        (game.GameState == GameState.Player1Move && swap))
                         p1win++;
                     break;
                 }
